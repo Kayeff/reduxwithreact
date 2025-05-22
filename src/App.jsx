5a4a8c4471fa133b6ed34edbd994e54b929fedec
Kaif <kaifsaiyed896@gmail.com>
2025-05-22 09:32:54 +0530

    add another piece of state to show/hide counter

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import Counter from "./components/Counter";
 
 export default function App() {
   return (
-    <main className="w-full min-h-screen bg-rich-black text-seasalt font-geist flex items-center justify-center">
+    <main className="w-full min-h-screen bg-rich-black text-seasalt font-geist flex items-center justify-center flex-col gap-10">
       <Counter />
     </main>
   );
--- src/store/index.js
+++ src/store/index.js
@@ -1,9 +1,23 @@
 import { createStore } from "redux";
 
-function reducer(state = { counter: 0 }, action) {
-  if (action.type === "increment") return { counter: state.counter + 1 };
-  if (action.type === "decrement") return { counter: state.counter - 1 };
-  if (action.type === "reset") return { counter: 0 };
+const initialValue = { counter: 0, showCounter: false };
+
+function reducer(state = initialValue, action) {
+  if (action.type === "increment")
+    return { counter: state.counter + 1, showCounter: state.showCounter };
+  if (action.type === "increase")
+    return {
+      counter: state.counter + action.payload,
+      showCounter: state.showCounter,
+    };
+  if (action.type === "decrement")
+    return { counter: state.counter - 1, showCounter: state.showCounter };
+  if (action.type === "reset")
+    return { counter: 0, showCounter: state.showCounter };
+
+  if (action.type === "show") {
+    return { showCounter: !state.showCounter, counter: state.counter };
+  }
   return state;
 }
 
